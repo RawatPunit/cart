cf643bf411abfd4a05f21b44b91ee6342f923010
Punit <rawatpunit928@gmail.com>
2023-07-05 12:38:39 +0530

    Update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import './App.css';
-import CartItem from './CartItem'
+import CartItem from './CartItem';
+import Cart from './Cart';
 function App() {
   return (
-    <div className="App">
-      <h1>Cart</h1>
+    <div className="App"> 
       <CartItem/>
     </div>
   );
--- src/CartItem.js
+++ src/CartItem.js
@@ -1,73 +1,85 @@
 import React from "react";
+import Cart from "./Cart";
 
 class CartItem extends React.Component{
-    constructor(){
-        super(); //this will call the const of the parent class which is (reactcomponent) othewise error
-        this.state= {
-            price : 999,
-            title : 'Phone',
-            qty : 1,
-            img: ''
-        }
-        //this.increseQuamtity = this.increaseQuantity.bind(this)
-    }
-    increaseQuantity=()=>{
-        // this.state.qty +=1;
-        console.log("this",this.state);
-        //setState form 1
-        // this.setState({
-        //     qty : this.state.qty + 1
-        // })
+    
+    // // ----------------below state has been moved to Cart.js-------------
+    // // constructor(){
+    // //     super(); //this will call the const of the parent class which is (reactcomponent) othewise error
+    // //     this.state= {
+    // //         price : 999,
+    // //         title : 'Phone',
+    // //         qty : 1,
+    // //         img: ''
+    // //     }
+    // //     //this.increseQuamtity = this.increaseQuantity.bind(this)
+    // // }
+    // // -------------------------------------------------
+    // increaseQuantity=()=>{
+    //     // this.state.qty +=1;
+    //     console.log("this",this.state);
+    //     //setState form 1
+    //     // this.setState({
+    //     //     qty : this.state.qty + 1
+    //     // })
 
-        // setState form 2 --------- if prev satte is req. use this
-        this.setState((prevState)=>{
-            return{
-                qty : prevState.qty + 1
-            }
-        });
-    }
-    decreaseQuantity=()=>{
-        const{qty} =this.state;
-        if(qty==0){
-            return;
-        }
-        // setState form 2 --------- if prev satte is req. use this
-        this.setState((prevState)=>{
-            return{
-                qty : prevState.qty - 1
-            }
-        });
-    }
+    //     // setState form 2 --------- if prev satte is req. use this
+    //     this.setState((prevState)=>{
+    //         return{
+    //             qty : prevState.qty + 1
+    //         }
+    //     });
+    // }
+    // decreaseQuantity=()=>{
+    //     const{qty} =this.state;
+    //     if(qty==0){
+    //         return;
+    //     }
+    //     // setState form 2 --------- if prev satte is req. use this
+    //     this.setState((prevState)=>{
+    //         return{
+    //             qty : prevState.qty - 1
+    //         }
+    //     });
+    // }
+    
     render(){
-        const { price,title,qty } = this.state;  // this will call from the const state-->thus can write dierctlty
+        console.log('this.props',this.props);
+        const { price,title,qty } = this.props.product;  // this will call from the const state-->thus can write dierctlty
         // title insted of this.state.title ;  sabko ek hi mei bula diya
+        const {product, onIncreaseQuantity, onDecreaseQuantity,onDelete } = this.props;
         return (
             <div className="cart-item">
+                {this.props.jsx}
                 <div className="left-block">
-                    <img style={style.image}/>
+                    <img style={styles.image}/>
                 </div>
                 <div className="right-block">
                     <div style={ { fontSize : 25 }} >{title}</div>
-                    <div style={ { color : '#777'  }} >{price}</div>
-                    <div style={ { color : '#777'  }} >{qty}</div>
+                    <div style={ { color : '#777'  }} >Rs. {price}</div>
+                    <div style={ { color : '#777'  }} >Qty: {qty}</div>
                     <div className="cart-items-actions">
                         {/*Button */}
                         <img 
                             alt="increase"
                             className="action-icons" 
                             src="https://cdn-icons-png.flaticon.com/128/3914/3914337.png" 
-                            onClick={this.increaseQuantity}
+                            // onClick={this.increaseQuantity}
+                            onClick={() => this.props.onIncreaseQuantity(product)}
                          />
                         <img 
                             alt="decrease" 
                             className="action-icons" 
                             src="https://cdn-icons-png.flaticon.com/128/10469/10469730.png"
-                            onClick={this.increaseQuantity}
+                            // onClick={this.decreaseQuantity}
+                            onClick={() => onDecreaseQuantity(product)}
+                            
                         />
                         <img 
                             alt="delete" 
                             className="action-icons"
                             src="https://cdn-icons-png.flaticon.com/128/3917/3917439.png"
+                            onClick={() => onDelete(product.id)}
                         />
                     </div>
                 </div>
@@ -77,11 +89,12 @@ class CartItem extends React.Component{
 }
 
 //cant use direct css hence used in the form of objects
-const style ={
+const styles ={
     image : {
         height : 110,
         width : 110,
-        borderRadius : 4
+        borderRadius : 4,
+        background : '#ccc'
     }
 }
 
